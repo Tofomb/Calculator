c6ca66c8c9cdad23bced3008ae6d360b1c782c9d
Tofomb <tobias.c.forsberg@gmail.com>
2022-04-08 09:21:50 +0200

    Made some more tests with [Theory]

--- CalcTest/UnitTest1.cs
+++ CalcTest/UnitTest1.cs
@@ -9,6 +9,7 @@ namespace CalcTest
         public void TestAddition()
         {
             Assert.Equal(10, Program.Addition(5, 5));
+            Assert.Equal(-10, Program.Addition(5, -15));
         }
         [Fact]
         public void TestAdditionArray()
@@ -21,7 +22,9 @@ namespace CalcTest
         [InlineData(1, 2, 3)]
         [InlineData(-4, -6, -10)]
         [InlineData(-2, 2, 0)]
-        public void AdditionTheory(int value1, int value2, int expected)
+        [InlineData(-2.5, 2.6, -2.5+2.6)]
+
+        public void AdditionTheory(double value1, double value2, double expected)
         {
             var result = Program.Addition(value1, value2);
             Assert.Equal(expected, result);
@@ -47,11 +50,25 @@ namespace CalcTest
             Assert.False(result.Division0);
         }
 
+
+
         [Fact]
         public void Div0()
         {
             var result = Program.Division(5, 0);
             Assert.True(result.Division0);
+        }
+
+
+        [Theory]
+        [InlineData(1, 2, false , (double)1/2)]
+        [InlineData(7, 0, true, 0)]
+        [InlineData(10, 2, false, 5)]
+        public void TheoryDivision(double value1, double value2, bool div0, double expected)
+        {
+            var result = Program.Division(value1, value2);
+            Assert.Equal(div0, result.Division0);
+            Assert.Equal(expected, result.Result);
         }
 
         [Fact]
